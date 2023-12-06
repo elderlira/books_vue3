d650de79762f9999507a21e5a543055d3a70c152
elderlira <elderfranca@hotmail.com>
2023-12-06 09:14:06 -0300

    project book

---
+++ src/components/Books.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="books-list">
+    <div class="book" v-for="(b, index) in books" :key="index">
+      <div class="book-cover">
+        <img :src="b.cover" />
+
+        <button :class="{ isRead: b.isRead }" @click="$emit('lido', b.id)">
+          <i class="fa-solid fa-eye"></i>
+          <span>{{ b.isRead ? "Já li" : "Não li" }}</span>
+        </button>
+      </div>
+
+      <div class="book-details">
+        <p class="book-author">{{ b.author }}</p>
+        <h3 class="book-title">{{ b.title }}</h3>
+        <p><i class="fa-solid fa-hashtag icon"></i> {{ b.isbn }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["books"]);
+</script>
---
+++ src/components/Progress.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>progresso {{ progress }}</div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const props = defineProps(["progresso"]);
+
+const progress = computed(() => {
+  return props.progresso.filter((b) => b.isRead).length;
+});
+</script>
